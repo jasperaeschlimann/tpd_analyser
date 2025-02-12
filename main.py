47bb10ce5ec1e8cbf80d2e7c85c114624a76970e
jasperaeschlimann <jasper.aeschlimann@outlook.com>
2025-02-12 14:08:20 +0000

    Window icons now handled in the window classes

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 import sys
 from PyQt5.QtWidgets import QApplication
-from PyQt5.QtGui import QIcon
 from gui.main_window import MainWindow
 
 def main():
     # Initialize the application
     app = QApplication(sys.argv)
-    app.setWindowIcon(QIcon("resources/TPD.ico"))
+
     # Create an instance of the main window
     window = MainWindow()
     window.show()
@@ -14,5 +13,6 @@ def main():
     # Execute the application
     sys.exit(app.exec_())
 
+# Ensures application only executes when main.py is directly run
 if __name__ == "__main__":
     main()
